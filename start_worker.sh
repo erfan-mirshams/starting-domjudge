79fa75d1279b301b476af0a6b1636beb9a6e72e8
Erfan Mirshams <erfan.mirshams@divar.ir>
2024-11-24 22:03:22 +0330

    chore: remove previous method scripts

--- start_worker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DOMJUDGE_VERSION=latest
-WORKER_ID=$1 #number (id) of judgehost
-SERVER_URL="your_url:12345/" #domserver url and port
-
-WORKER_PASSWORD="judge_pass" #judgehost user account password in the server
-
-sudo docker run -it --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name judgehost-$(whoami)-$WORKER_ID --link domserver:domserver --hostname judgedaemon-$(whoami)-$WORKER_ID -e DAEMON_ID=$WORKER_ID -e DOMSERVER_BASEURL=$SERVER_URL -e JUDGEDAEMON_PASSWORD=$WORKER_PASSWORD -e CONTAINER_TIMEZONE=Asia/Tehran domjudge/judgehost:$DOMJUDGE_VERSION
